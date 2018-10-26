787223a6fde669f6708ef920447a53ce994daf04
Michael Sypniewski <mjsyp@umich.edu>
2018-10-26 10:04:01 -0400

    Updated and corrected launch, topic, and parameter formats

--- wamv_ws/src/wamv_control/src/pid_interpreter.py
+++ wamv_ws/src/wamv_control/src/pid_interpreter.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python
 
-import sys
 import rospy
 from std_msgs.msg import Int16
+from std_msgs.msg import Float64
 
 def sign(x):
-    # cool, huh?
+    """ Returns the sign of 'x'. Pretty cool, huh?
+    """
     return (1, -1)[x < 0]
 
 class PIDInterpreter:
+    """ The interpreter between the outputs of the PID controllers and the
+        inputs of the main motors. Responsible for performing the necessary
+        logic to transform 'heading' and 'speed' control efforts into port and
+        starboard main motor inputs.
+
+        # Parameters
+        - maximum_output: The maximum output of the interpreter, aka the
+            maximum main motor input. Prioritizes heading controller. Default
+            is 127.
+        - loop_rate: The rate of the main loop, in Hz. Default is 10 Hz.
+
+        # Subscribed Topics
+        - topic_from_heading_controller (Float64): The control effort being
+            published by the heading controller.
+        - topic_from_speed_controller (Float64): The control effort being
+            published be the speed controller.
+
+        # Published Topics
+        - topic_to_port_motor (Int16): The port-side main motor input.
+        - topic_to_strbrd_motor (Int16): The starboard-side main motor input.
+    """
+
     def __init__(self):
-        node_name = rospy.get_param('~node_name', 'interpreter_node')
-        rospy.init_node(node_name, log_level=rospy.DEBUG)
-
-        # Set up publishers
-        port_motor_topic = rospy.get_param(
-            '~port_motor_topic', 'port_motor_speed'
-        )
-        self.port_motor_publisher = rospy.Publisher(
-            port_motor_topic, Int16, queue_size=1000
-        )
-
-        strbrd_motor_topic = rospy.get_param(
-            '~strbrd_motor_topic', 'strbrd_motor_speed'
-        )
-        self.strbrd_motor_publisher = rospy.Publisher(
-            strbrd_motor_topic, Int16, queue_size=1000
-        )
+        rospy.init_node('pid_interpreter_node', log_level=rospy.DEBUG)
+        self.maximum_output = rospy.get_param('~maximum_output', 127)
 
         # Set up subscribers
-        speed_control_effort_topic = rospy.get_param(
-            '~speed_control_effort_topic', 'speed_control_effort'
-        )
-        rospy.Subscriber(
-            speed_control_effort_topic, Int16, self.speed_callback
-        )
-
-        heading_control_effort_topic = rospy.get_param(
-            '~heading_control_effort_topic', 'heading_control_effort'
-        )
-        rospy.Subscriber(
-            heading_control_effort_topic, Int16, self.heading_callback
-        )
-
-        # Set up others
-        self.maximum = rospy.get_param('~maximum', 127)
         self.heading_control_effort = 0
+        rospy.Subscriber(
+            'topic_from_heading_controller', Float64, self.heading_callback
+        )
         self.speed_control_effort = 0
+        rospy.Subscriber(
+            'topic_from_speed_controller', Float64, self.speed_callback
+        )
+
+        # Set up publishers
+        self.port_motor_publisher = rospy.Publisher(
+            'topic_to_port_motor', Int16, queue_size=1000
+        )
+        self.strbrd_motor_publisher = rospy.Publisher(
+            'topic_to_strbrd_motor', Int16, queue_size=1000
+        )
 
     def speed_callback(self, msg):
         """ Velocities fall in the range [-127,127] with -127 representing full
@@ -61,7 +67,7 @@ class PIDInterpreter:
         self.heading_control_effort = msg.data
 
     def process(self):
-        """ Calculates and sends the motor speed signals.
+        """ Calculates and publishes the main motor input values.
 
             The motor works with values between [0,254] with 0 being full
             reverse and 254 being full forward. The translation happens like so:
@@ -78,19 +84,20 @@ class PIDInterpreter:
         strbrd_motor_speed = -self.heading_control_effort
 
         max_ = max(port_motor_speed, strbrd_motor_speed)
-        if max_ > self.maximum:
-            max_ = self.maximum
+        if max_ > self.maximum_output:
+            max_ = self.maximum_output
             port_motor_speed = max_ * sign(port_motor_speed)
             strbrd_motor_speed = max_ * sign(strbrd_motor_speed)
 
         incr = 127
         if self.speed_control_effort > 0:
-            incr += min(self.speed_control_effort, self.maximum - max_)
+            incr += min(self.speed_control_effort, self.maximum_output - max_)
         else:
-            incr += max(self.speed_control_effort, max_ - self.maximum)
+            incr += max(self.speed_control_effort, max_ - self.maximum_output)
 
-        port_motor_speed += incr
-        strbrd_motor_speed += incr
+        # Convert from Float64 to Int16
+        port_motor_speed = int(port_motor_speed + incr)
+        strbrd_motor_speed = int(strbrd_motor_speed + incr)
 
         self.port_motor_publisher.publish(port_motor_speed)
         self.strbrd_motor_publisher.publish(strbrd_motor_speed)
@@ -98,8 +105,9 @@ class PIDInterpreter:
 
 if __name__ == '__main__':
     pid_interpreter = PIDInterpreter()
-    loop_rate = rospy.get_param('~loop_rate', 10)
-    rate = rospy.Rate(loop_rate)
+    rate = rospy.Rate(
+        rospy.get_param('~loop_rate', 10)
+    )
     while not rospy.is_shutdown():
         pid_interpreter.process()
         rate.sleep()
