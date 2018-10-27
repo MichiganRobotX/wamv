3ace26015cf7c6915dceb95b6b65714eae249ef8
Conner Goodrum <cgoodrum@umich.edu>
2018-10-27 16:33:10 -0400

    Rewrote heading unit converter and updated controls.launch with new states

--- wamv_ws/src/wamv_navigation/src/heading_unit_converter.py
+++ wamv_ws/src/wamv_navigation/src/heading_unit_converter.py
@@ -3,6 +3,7 @@
 import rospy
 import math
 from std_msgs.msg import Float64
+from sensor_msgs.msg import Imu
 
 class HeadingConverter():
 
@@ -11,42 +12,40 @@ class HeadingConverter():
         rospy.init_node('heading_unit_converter_node', log_level = rospy.DEBUG)
 
         # Set up subscribers
-        rospy.Subscriber('an_device/Imu/orientation/w', Float64, self.w_callback)
-        rospy.Subscriber('an_device/Imu/orientation/x', Float64, self.x_callback)
-        rospy.Subscriber('an_device/Imu/orientation/y', Float64, self.y_callback)
-        rospy.Subscriber('an_device/Imu/orientation/z', Float64, self.z_callback)
+	    rospy.Subscriber('an_device/Imu', Imu, self.test_callback)
 
         # Set up publisher
         self.pub = rospy.Publisher('current_heading', Float64, queue_size = 1000)
 
         # Initialize subscribed topic values
+
         self.w = Float64(0.0)
         self.x = Float64(0.0)
         self.y = Float64(0.0)
         self.z = Float64(0.0)
 
-    def w_callback(self, msg):
-        self.w = msg.data
+    def test_callback(self, msg):
+        self.w.data = msg.orientation.w
+        self.x.data = msg.orientation.x
+        self.y.data = msg.orientation.y
+        self.z.data = msg.orientation.z
 
-    def x_callback(self, msg):
-        self.x = msg.data
-
-    def y_callback(self, msg):
-        self.y = msg.data
-
-    def z_callback(self, msg):
-        self.z = msg.data
+    def quat_to_deg(self, w, x, y, z):
+    	t1 = 2.0 * (w * z + x * y)
+        t2 = 1.0 - 2.0 * (y * y + z * z)
+        deg_heading = math.degrees(math.atan2(t1, t2))
+	    if deg_heading < 0.0:
+		          deg_heading +=360
+	    return deg_heading
 
     def calculate(self):
-        t1 = +2.0 * (self.w * self.z + self.x * self.y)
-        t2 = +1.0 - 2.0 * (self.y * self.y + self.z * self.z)
-        deg_heading = math.degrees(math.atan2(t1, t2))
-
+        deg_heading = self.quat_to_deg(self.w.data, self.x.data, self.y.data, self.z.data)
+        rospy.loginfo("Current Heading = " + str(deg_heading))
         self.pub.publish(deg_heading)
 
 if __name__ == '__main__':
     heading_converter = HeadingConverter()
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(40)
     while not rospy.is_shutdown():
         heading_converter.calculate()
         rate.sleep()
