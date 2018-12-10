5700cce61f441936d31f29702b1ad17cd90203d4
Conner Goodrum <cgoodrum@umich.edu>
2018-12-10 09:16:53 -1000

    initial straight line task code

---
+++ wamv_ws/src/wamv_tasks/src/straight_line.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, Point, Quaternion, Twist
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from tf.transformations import quaternion_from_euler
+from visualization_msgs.msg import Marker
+from math import radians, pi
+
+class StraightLineTask():
+    def __init__(self):
+        rospy.init_node('straight_line', anonymous=False)
+
+        rospy.on_shutdown(self.shutdown)
+
+        distance = rospy.get_param('~distance', 10.0)
+        # Create a list to hold the target quaternions (orientations)
+        quaternions = list()
+
+        # First define Euler angle relative to base link
+        angle = 0.0
+
+        # Then convert the angle to quaternions
+        q_angle = quaternion_from_euler(0, 0, angle, axes='sxyz')
+        q = Quaternion(q_angle)
+        quaternions.append(q)
+
+        # Create a list to hold the waypoint poses
+        waypoints = list()
+
+        # Append each of the four waypoints to the list.  Each waypoint
+        # is a pose consisting of a position and orientation in the map frame.
+        waypoints.append(Pose(Point(distance, 0.0, 0.0), quaternions[0]))
+
+        # Initialize the visualization markers for RViz
+        self.init_markers()
+
+        # Set a visualization marker at each waypoint
+        for waypoint in waypoints:
+            p = Point()
+            p = waypoint.position
+            self.markers.points.append(p)
+
+        # Publisher to manually control the robot (e.g. to stop it)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist)            ## CHECK THIS
+
+        # Subscribe to the move_base action server
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+
+        rospy.loginfo("Waiting for move_base action server...")
+
+        # Wait 60 seconds for the action server to become available
+        self.move_base.wait_for_server(rospy.Duration(60))
+
+        rospy.loginfo("Connected to move base server")
+        rospy.loginfo("Starting navigation test")
+
+        # Initialize a counter to track waypoints
+        i = 0
+
+        # Cycle through the four waypoints
+        while i <= len(waypoints) and not rospy.is_shutdown():
+            # Update the marker display
+            self.marker_pub.publish(self.markers)
+
+            # Intialize the waypoint goal
+            goal = MoveBaseGoal()
+
+            # Use the map frame to define goal poses
+            goal.target_pose.header.frame_id = 'base_link'
+
+            # Set the time stamp to "now"
+            goal.target_pose.header.stamp = rospy.Time.now()
+
+            # Set the goal pose to the i-th waypoint
+            goal.target_pose.pose = waypoints[i]
+
+            # Start the robot moving toward the goal
+            self.move(goal)
+
+            i += 1
+
+    def move(self, goal):
+            # Send the goal pose to the MoveBaseAction server
+            self.move_base.send_goal(goal)
+
+            # We made it!
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                rospy.loginfo("Goal succeeded!")
+
+    def init_markers(self):
+        # Set up our waypoint markers
+        marker_scale = 0.2
+        marker_lifetime = 0 # 0 is forever
+        marker_ns = 'waypoints'
+        marker_id = 0
+        marker_color = {'r': 1.0, 'g': 0.7, 'b': 1.0, 'a': 1.0}
+
+        # Define a marker publisher.
+        self.marker_pub = rospy.Publisher('waypoint_markers', Marker)
+
+        # Initialize the marker points list.
+        self.markers = Marker()
+        self.markers.ns = marker_ns
+        self.markers.id = marker_id
+        self.markers.type = Marker.SPHERE_LIST
+        self.markers.action = Marker.ADD
+        self.markers.lifetime = rospy.Duration(marker_lifetime)
+        self.markers.scale.x = marker_scale
+        self.markers.scale.y = marker_scale
+        self.markers.color.r = marker_color['r']
+        self.markers.color.g = marker_color['g']
+        self.markers.color.b = marker_color['b']
+        self.markers.color.a = marker_color['a']
+
+        self.markers.header.frame_id = 'map'
+        self.markers.header.stamp = rospy.Time.now()
+        self.markers.points = list()
+
+    def shutdown(self):
+        rospy.loginfo("Stopping the robot...")
+        # Cancel any active goals
+        self.move_base.cancel_goal()
+        rospy.sleep(2)
+        # Stop the robot
+        self.cmd_vel_pub.publish(Twist())
+        rospy.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        StraightLineTask()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation test finished.")
